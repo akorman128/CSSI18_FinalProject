0b6c33c2c20a15f6199d10f016c65f6841dee871
Ben Levy <blevy365@gmail.com>
2018-07-30 16:54:13 -0700

    main.py

--- main.py
+++ main.py
@@ -2,33 +2,47 @@ import webapp2
 import jinja2
 import os
 import random
+from google.appengine.api import users
+from google.appengine.ext import ndb
 
-#remember, you can get this by searching for jinja2 google app engine
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+class Account(ndb.Model):
+    id = ndb.StringProperty()
+    points = ndb.FloatProperty()
 
-class ___Handler(webapp2.RequestHandler):
+class UserProfileHandler(webapp2.RequestHandler):
     def get(self):
-        results_template = JINJA_ENVIRONMENT.get_template('templates/______.html')
-        self.response.write(results_template.render())
-    #add a post method
-    #def post(self):
-    def post(self):
+        user = users.get_current_user()
+        nickname = user.nickname()
+        logout_url = users.create_logout_url('/')
+        greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(nickname, logout_url)
+
+        if len(Account.query(Account.id == user.user_id()).fetch()) == 0:
+            new_user = Account()
+            new_user.id = user.user_id()
+            new_user.points = 0
+            new_user_key = new_user.put()
 
         template_vars = {
-
+            'nickname': nickname,
+            'logout': logout_url,
+            'greeting': greeting,
+            'points': Account.query(Account.id == user.user_id()).fetch()[0].points
         }
 
-        results_template = JINJA_ENVIRONMENT.get_template('templates/_______.html')
-        self.response.write(results_template.render(template_vars))
+        profile_template = JINJA_ENVIRONMENT.get_template('templates/html/user_profile.html')
+        self.response.write(profile_template.render(template_vars))
 
+
+    def post(self):
+        pass
 #the route mapping
 app = webapp2.WSGIApplication([
     #this line routes the main url ('/')  - also know as
     #the root route - to the Fortune Handler
-    ('/', _____),
-    ('/_____', ___Handler),
+    ('/user', UserProfileHandler),
 ], debug=True)
