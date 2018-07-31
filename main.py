56653e59281c37db53fd197906dd95898ebfb1e8
Ben Levy <blevy365@gmail.com>
2018-07-30 18:09:47 -0700

    n

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 class Account(ndb.Model):
     id = ndb.StringProperty()
     points = ndb.FloatProperty()
+    project_keys = ndb.StringProperty(repeated=True)
+
+class Project(ndb.Model):
+    title = ndb.StringProperty()
+    date = ndb.DateTimeProperty()
+    area = ndb.StringProperty()
+    description = ndb.StringProperty()
 
 class UserProfileHandler(webapp2.RequestHandler):
     def get(self):
@@ -23,7 +30,7 @@ class UserProfileHandler(webapp2.RequestHandler):
         greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(nickname, logout_url)
 
         # If no account exists, make one
-        if len(Account.query(Account.id == user.user_id()).fetch()) == 0:
+        if Account.query(Account.id == user.user_id()) is None:
             new_user = Account()
             new_user.id = user.user_id()
             new_user.points = 0
@@ -33,19 +40,45 @@ class UserProfileHandler(webapp2.RequestHandler):
         template_vars = {
             'nickname': nickname,
             'logout': logout_url,
-            'greeting': greeting,
+            'logout_url': logout_url,
             'points': Account.query(Account.id == user.user_id()).fetch()[0].points
         }
 
         profile_template = JINJA_ENVIRONMENT.get_template('templates/html/user_profile.html')
         self.response.write(profile_template.render(template_vars))
 
+class CreateProjectHandler(webapp2.RequestHandler):
+    def get(self):
+        create_template = JINJA_ENVIRONMENT.get_template('templates/html/create.html')
+        self.response.write(create_template.render())
 
     def post(self):
-        pass
+        # Sign in was required, so get user info from Google App Engine
+        user = users.get_current_user()
+        nickname = user.nickname()
+        logout_url = users.create_logout_url('/')
+        greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(nickname, logout_url)
+
+        title = self.request.get('title')
+        #date = self.request.get('date')
+        area = self.request.get('area')
+        description = self.request.get('description')
+
+        new_project = Project()
+        new_project.title = title
+        #new_project.date = date
+        new_project.area = area
+        new_project.description = description
+        new_project_key = new_project.put()
+
+        current_user = Account.query(Account.id == user.user_id())
+        print(current_user.filter(current_user.id)
+        current_user.project_keys = new_project_key
+
 #the route mapping
 app = webapp2.WSGIApplication([
     #this line routes the main url ('/')  - also know as
     #the root route - to the Fortune Handler
     ('/user', UserProfileHandler),
+    ('/create', CreateProjectHandler),
 ], debug=True)
